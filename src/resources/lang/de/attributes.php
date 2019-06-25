cddd978ba31aa14feca5d4d2046eb4241dccb302
Gerald Buttinger <gerald@code.florist>
2019-06-25 15:33:31 +0200

    add german default attribute translations

---
+++ src/resources/lang/de/attributes.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    | (taken from caouecs/Laravel-lang)
+    |
+    */
+
+    'name'                  => 'Name',
+    'username'              => 'Benutzername',
+    'email'                 => 'E-Mail-Adresse',
+    'first_name'            => 'Vorname',
+    'last_name'             => 'Nachname',
+    'password'              => 'Passwort',
+    'password_confirmation' => 'Passwort-Bestätigung',
+    'city'                  => 'Stadt',
+    'country'               => 'Land',
+    'address'               => 'Adresse',
+    'phone'                 => 'Telefonnummer',
+    'mobile'                => 'Handynummer',
+    'age'                   => 'Alter',
+    'sex'                   => 'Geschlecht',
+    'gender'                => 'Geschlecht',
+    'day'                   => 'Tag',
+    'month'                 => 'Monat',
+    'year'                  => 'Jahr',
+    'hour'                  => 'Stunde',
+    'minute'                => 'Minute',
+    'second'                => 'Sekunde',
+    'title'                 => 'Titel',
+    'content'               => 'Inhalt',
+    'description'           => 'Beschreibung',
+    'excerpt'               => 'Auszug',
+    'date'                  => 'Datum',
+    'time'                  => 'Uhrzeit',
+    'available'             => 'verfügbar',
+    'size'                  => 'Größe',
+    'message'               => 'Nachricht'
+
+];
